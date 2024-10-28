0ada3158c4caf94cbe2b1f23eec0e690e91bc7ad
lijinbu21 <891284194@qq.com>
2024-10-28 23:41:23 +0800

    Auto-update: 2024-10-28 23:41:23

--- auto_push.ps1
+++ auto_push.ps1
@@ -1,5 +1,20 @@
 [Console]::OutputEncoding = [System.Text.Encoding]::UTF8
 
+# 设置日志文件路径
+$logFile = "D:\ALLINONE\klara-notes\auto_push_log.txt"
+
+# 写入日志的函数
+function Write-Log {
+    param (
+        [string]$message
+    )
+    $timestamp = (Get-Date).ToString("yyyy-MM-dd HH:mm:ss")
+    "$timestamp - $message" | Out-File -FilePath $logFile -Append
+}
+
+# 启动日志记录
+Write-Log "Start Excuting your task -- update notes :$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss')"
+
 # 设置 Git 仓库路径
 $repoPath = "D:\ALLINONE\klara-notes"
 
@@ -20,7 +35,9 @@ if ($changes) {
     # 推送到远程仓库
     git push origin main
 
-    Write-Output "OK, push to GitHub"
+    Write-Log "OK, push to GitHub"
 } else {
-    Write-Output "Nothing new need to be pushed"
+    Write-Log "Nothing new need to be pushed"
 }
+
+Write-Log "Fish your task"
